3325682f0a6766c45c66b55dedc6b1bc45b85324
prerana <prerana.chaks@gmail.com>
2024-12-02 16:06:29 +0800

    Initial Commit

---
+++ StableDiffusion-CoreML/StableDiffusion_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  StableDiffusion_CoreMLApp.swift
+//  StableDiffusion-CoreML
+//
+//  Created by Prerana Chakraborty on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StableDiffusion_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StableDiffusion-CoreMLTests/StableDiffusion_CoreMLTests.swift
@@ -0,0 +1,17 @@
+//
+//  StableDiffusion_CoreMLTests.swift
+//  StableDiffusion-CoreMLTests
+//
+//  Created by Prerana Chakraborty on 02/12/24.
+//
+
+import Testing
+@testable import StableDiffusion_CoreML
+
+struct StableDiffusion_CoreMLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
